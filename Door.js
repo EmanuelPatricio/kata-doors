a322ae3a5a454a7aefcf75cfeb945e63ae38ffd0
EmanuelPatricio <emanuelptr81@gmail.com>
2021-12-06 10:46:56 -0400

    Getters and Static methods created

--- Door.js
+++ Door.js
@@ -1,17 +1,38 @@
-'use strict'
+'use strict';
 
 module.exports = class Door {
   constructor(
     key = '123',
-    isOpen = true,
-    isClosed = false,
-    isUnlocked = true,
-    isLocked = false
+    isOpen = false,
+    isClosed = true,
+    isUnlocked = false,
+    isLocked = true
   ) {
     this.key = key;
     this.isOpen = isOpen;
     this.isClosed = isClosed;
     this.isUnlocked = isUnlocked;
     this.isLocked = isLocked;
+  }
+
+  IsOpen() { return this.isOpen; }
+  IsClosed() { return this.isClosed; }
+  IsUnlocked() { return this.isUnlocked; }
+  IsLocked() { return this.isLocked; }
+
+  static OpenAndUnlocked(key) {
+    return new Door(key, true, false, true, false);
+  }
+
+  static OpenAndLocked(key) {
+    return new Door(key, true, false, false, true);
+  }
+
+  static CloseAndUnlocked(key) {
+    return new Door(key, false, true, true, false);
+  }
+
+  static CloseAndLocked(key) {
+    return new Door(key, false, true, false, true);
   }
 }
--- Door.test.js
+++ Door.test.js
@@ -5,6 +5,38 @@ describe('Door', () => {
   const door = new Door();
 
   it('New door object is created', () => {
-    expect(door).toEqual({ "isOpen": true, "isClosed": false, "isUnlocked": true, "isLocked": false, "key": '123' });
+    expect(door).toEqual({ "isOpen": false, "isClosed": true, "isUnlocked": false, "isLocked": true, "key": '123' });
+  });
+
+  it('Getting IsOpen value', () => {
+    expect(door.IsOpen()).toBe(false);
+  });
+
+  it('Getting IsClosed value', () => {
+    expect(door.IsClosed()).toBe(true);
+  });
+
+  it('Getting IsUnlocked value', () => {
+    expect(door.IsUnlocked()).toBe(false);
+  });
+
+  it('Getting IsLocked value', () => {
+    expect(door.IsLocked()).toBe(true);
+  });
+
+  it('OpenAndUnlocked calls return Door', () => {
+    expect(Door.OpenAndUnlocked('123')).toEqual({ "isOpen": true, "isClosed": false, "isUnlocked": true, "isLocked": false, "key": '123' });
+  });
+
+  it('OpenAndLocked calls return Door', () => {
+    expect(Door.OpenAndLocked('123')).toEqual({ "isOpen": true, "isClosed": false, "isUnlocked": false, "isLocked": true, "key": '123' });
+  });
+
+  it('CloseAndUnlocked calls return Door', () => {
+    expect(Door.CloseAndUnlocked('123')).toEqual({ "isOpen": false, "isClosed": true, "isUnlocked": true, "isLocked": false, "key": '123' });
+  });
+
+  it('CloseAndLocked calls return Door', () => {
+    expect(Door.CloseAndLocked('123')).toEqual({ "isOpen": false, "isClosed": true, "isUnlocked": false, "isLocked": true, "key": '123' });
   });
 });
